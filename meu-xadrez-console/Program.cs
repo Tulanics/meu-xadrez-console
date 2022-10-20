c3e9fbd9bf948d8d2e8363cf584c5fbb62cf9379
tulanics@gmail.com <tulani.santos@raizen.com>
2022-10-20 14:21:12 -0300

    Projeto criado

---
+++ meu-xadrez-console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace meu_xadrez_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
